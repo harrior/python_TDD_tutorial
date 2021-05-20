3bd902c9540a633dc1bcc2e8aefd9b950a9e393a
harrior <harrior@users.noreply.github.com>
2021-05-20 15:22:54 +0300

    FT now check a posibility to enter list's element

--- functional_testing.py
+++ functional_testing.py
@@ -1,5 +1,9 @@
-from selenium import webdriver
+import time
 import unittest
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
 
 class NewVisitorTest(unittest.TestCase):
     """ new visitor test """
@@ -19,14 +23,24 @@ class NewVisitorTest(unittest.TestCase):
 
         # Он видит заголовок страницы To-Do list
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Закончить тест!')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
         # Ему сразу предлагают ввести новую задачу
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter to-do item')
 
         # Он вводит "Купить перья павлина"
-
+        inputbox.send_keys('Купить перья павлина')
         # При нажатии Enter страница обновляется и теперь страница содержит "Купить перья павлина"
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
 
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Купить перья павлина' for row in rows)
+        )
         # Теперь он вводит "Сделать мушку из павлиньих перьев"
 
         # Страница снова обновляется и теперь показывает два элемента его списка
@@ -34,7 +48,7 @@ class NewVisitorTest(unittest.TestCase):
         # Сайт сгенерировал уникальный URL для этого списка?
 
         # Пользователь переходит по ссылке - список все еще на месте
-
+        self.fail('Закончить тест!')
 
 if __name__ == '__main__':
     unittest.main()
