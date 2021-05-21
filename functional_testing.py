47c2daa428f34f9ab0dd3a316a76135031a421de
harrior <harrior@users.noreply.github.com>
2021-05-21 09:39:47 +0300

    Add check second string in FT

--- functional_testing.py
+++ functional_testing.py
@@ -35,15 +35,19 @@ class NewVisitorTest(unittest.TestCase):
         # При нажатии Enter страница обновляется и теперь страница содержит "Купить перья павлина"
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
-
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Купить перья павлина' for row in rows)
-        ,'Новый элемент списка не появился в таблице')
+        self.assertIn('1: Купить перья павлина', [row.text for row in rows])
         # Теперь он вводит "Сделать мушку из павлиньих перьев"
-
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Сделать мушку из павлиньих перьев')
+        inputbox.send_keys(Keys.ENTER)
         # Страница снова обновляется и теперь показывает два элемента его списка
+        time.sleep(1)
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('1: Купить перья павлина', [row.text for row in rows])
+        self.assertIn('2: Сделать мушку из павлиньих перьев', [row.text for row in rows])
 
         # Сайт сгенерировал уникальный URL для этого списка?
 
