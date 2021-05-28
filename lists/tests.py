aeacfe1c58de4f890308e4a472b8f7d47d259a5d
Harrior <harrior@users.noreply.github.com>
2021-05-28 09:17:13 +0300

    new URL, view and template to display lists

--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,17 @@
 from django.test import TestCase
 from lists.models import Item
 
+class ListViewTest(TestCase):
+    def test_uses_list_template(self):
+        response = self.client.get('/lists/uniquest-list-in-the-world/')
+        self.assertTemplateUsed(response, 'list.html')
+
+    def test_displays_all_items(self):
+        Item.objects.create(text='itemey 1')
+        Item.objects.create(text='itemey 2')
+        response = self.client.get('/lists/uniquest-list-in-the-world/')
+        self.assertContains(response, 'itemey 1')
+        self.assertContains(response, 'itemey 2')
 
 class HomePageTest(TestCase):
     """тест домашней страницы"""
@@ -25,16 +36,8 @@ class HomePageTest(TestCase):
         """тест:переадресует после post запроса"""
         response = self.client.post('/', data={'item_text': 'A new list item'})
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'],'/')
+        self.assertEqual(response['location'],'/lists/uniquest-list-in-the-world/')
 
-    def test_display_all_list_items(self):
-        """тест: отображение всех элементов в списке"""
-        Item.objects.create(text='itemey 1')
-        Item.objects.create(text='itemey 2')
-
-        response = self.client.get('/')
-        self.assertIn('itemey 1', response.content.decode())
-        self.assertIn('itemey 2', response.content.decode())
 
 class ItemModelTest(TestCase):
     def test_saving_and_retrieving_items(self):
--- lists/views.py
+++ lists/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, redirect
 # from django.http import HttpResponse
-
 from lists.models import Item
 
 
@@ -9,6 +8,10 @@ def home_page(request):
     """домашняя страница"""
     if request.method == 'POST':
         Item.objects.create(text=request.POST['item_text'])
-        return redirect('/')
+        return redirect('/lists/uniquest-list-in-the-world/')
+    return render(request, 'home.html')
+
+def view_list(request):
+    """ представление списка """
     items = Item.objects.all()
-    return render(request, 'home.html', {'items': items})
+    return render(request, 'list.html', {'items': items})
