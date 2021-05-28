6faee78df61ebbbfc4370723e2467b5b4301b8ee
Harrior <harrior@users.noreply.github.com>
2021-05-28 11:26:06 +0300

    create List model

--- lists/models.py
+++ lists/models.py
@@ -1,6 +1,14 @@
 from django.db import models
-
-
 # Create your models here.
+
+
+class List(models.Model):
+    """список"""
+    pass
+
+
 class Item(models.Model):
+    """Элемент"""
     text = models.TextField(default='')
+    list = models.ForeignKey(List, default=None)
+
--- lists/views.py
+++ lists/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 # from django.http import HttpResponse
-from lists.models import Item
+from lists.models import Item, List
 
 
 # Create your views here.
@@ -8,11 +8,15 @@ def home_page(request):
     """домашняя страница"""
     return render(request, 'home.html')
 
+
 def view_list(request):
     """ представление списка """
     items = Item.objects.all()
     return render(request, 'list.html', {'items': items})
 
+
 def new_list(request):
-    Item.objects.create(text=request.POST['item_text'])
+    """create a new list"""
+    list_ = List.objects.create()
+    Item.objects.create(text=request.POST['item_text'], list=list_)
     return redirect('/lists/uniquest-list-in-the-world/')
